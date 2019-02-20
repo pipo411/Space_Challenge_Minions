93f73d389755cb6f37aba72d554b0c62bb195a6a
carledriss <carledriss@gmail.com>
2019-02-20 00:26:11 -0400

    Added changes to methods

--- SpaceChallenge/src/main/java/rocket/U1.java
+++ SpaceChallenge/src/main/java/rocket/U1.java
@@ -7,24 +7,25 @@ public class U1 extends Rocket {
     private final static int COST = 100000000;
     private final static int WEIGHT = 10000;
     private final static int MAX_WEIGHT = 18000;
-    Random random = new Random();
+    private final static int CURRENT_WEIGHT = 0;
+    private final static Random random = new Random();
 
 
     public U1() {
-        super(COST, WEIGHT, MAX_WEIGHT);
+        super(COST, WEIGHT, MAX_WEIGHT, CURRENT_WEIGHT);
     }
 
     @Override
     public boolean launch() {
         //Add launch logic chance to crash
-//        return Math.random() < (0.05 * (this.currentWeight / MAX_WEIGHT));
-        return random.nextBoolean();
+        return random.nextInt(2) < (0.05 * (this.currentWeight / MAX_WEIGHT));
+        //return random.nextBoolean();
     }
 
     @Override
     public boolean land() {
         //Add land logic chance to crash
-//        return Math.random() < (0.01 * (this.currentWeight / MAX_WEIGHT));
-        return random.nextBoolean();
+        return random.nextInt(2) < (0.01 * (this.currentWeight / MAX_WEIGHT));
+        //return random.nextBoolean();
     }
 }
