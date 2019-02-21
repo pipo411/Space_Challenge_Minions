ebd710fdfd50900d4e3afe89b11ff323ee16f3b7
pipo411 <ariel.gonzales@fundacion-jala.org>
2019-02-20 23:56:30 -0400

    Added probability rockets

--- SpaceChallenge/src/main/java/rocket/U2.java
+++ SpaceChallenge/src/main/java/rocket/U2.java
@@ -8,22 +8,20 @@ public class U2 extends Rocket {
     private final static int WEIGHT = 18000;
     private final static int MAX_WEIGHT = 29000;
     private final static int CURRENT_WEIGHT = 0;
-    private final static Random random = new Random();
+
     public U2() {
         super(COST, WEIGHT, MAX_WEIGHT, CURRENT_WEIGHT);
     }
 
     @Override
     public boolean launch() {
-        int number = random.nextInt(10);
-        double randomProb = number / 100.0;
-        return randomProb <= (0.04 * (this.currentWeight / MAX_WEIGHT));
+        double probability = (double) getCurrentWeight() / MAX_WEIGHT;
+        return Math.random() > 0.04 * probability;
     }
 
     @Override
     public boolean land() {
-        int number = random.nextInt(10);
-        double randomProb = number / 100.0;
-        return randomProb <= (0.08 * (this.currentWeight / MAX_WEIGHT));
+        double probability = (double) getCurrentWeight() / MAX_WEIGHT;
+        return Math.random() > 0.08 * probability;
     }
 }
